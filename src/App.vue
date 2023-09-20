d6d91252f24cdd3e88ff87747f7b187d811a9f2f
Rafael <rafaellf112@gmail.com>
2023-09-20 18:05:22 -0300

    Commit dos primeiros passos no Vue.js

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Emoji de joia" src="./assets/joinha.png">
+  <HelloWorld msg="Hello 🌎!"/>
+  <h1></h1>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Lobster', cursive;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #023d79;
+  margin-top: 60px;
+  font-size: 350%;
+  
+}
+</style>
